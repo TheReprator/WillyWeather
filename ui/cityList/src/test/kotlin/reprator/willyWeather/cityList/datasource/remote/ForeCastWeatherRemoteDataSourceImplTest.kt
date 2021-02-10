847ae906bf454b7ec66e0163a3f8d297767d6d09
Vikram Singh <mr.vikram004@gmail.com>
2021-02-10 22:01:39 +0530

    extension function test

--- ui/cityList/src/test/kotlin/reprator/willyWeather/cityList/datasource/remote/ForeCastWeatherRemoteDataSourceImplTest.kt
+++ ui/cityList/src/test/kotlin/reprator/willyWeather/cityList/datasource/remote/ForeCastWeatherRemoteDataSourceImplTest.kt
@@ -5,19 +5,27 @@ import com.google.common.truth.Truth
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.slot
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.single
 import kotlinx.coroutines.test.runBlockingTest
+import okhttp3.ResponseBody
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import reprator.willyWeather.base.useCases.ErrorResult
 import reprator.willyWeather.base.useCases.Success
+import reprator.willyWeather.base.useCases.WillyWeatherResult
+import reprator.willyWeather.base.util.toResult
 import reprator.willyWeather.cityList.TestFakeData.getFakeLocationModalDataList
 import reprator.willyWeather.cityList.TestFakeData.getFakeRemoteDataList
 import reprator.willyWeather.cityList.data.datasource.ForecastWeatherRemoteDataSource
+import reprator.willyWeather.cityList.datasource.remote.modal.ForecastLocationEntity
 import reprator.willyWeather.cityList.datasource.remote.remoteMapper.ForecastWeatherMapper
 import reprator.willyWeather.cityList.modals.LocationRequestModal
 import reprator.willyWeather.testUtils.MainCoroutineRule
@@ -48,8 +56,8 @@ class ForeCastWeatherRemoteDataSourceImplTest {
         MockKAnnotations.init(this, true)
 
         foreCastWeatherRemoteDataSource = ForeCastWeatherRemoteDataSourceImpl(
-            weatherApiService,
-            forecastWeatherMapper
+                weatherApiService,
+                forecastWeatherMapper
         )
     }
 
@@ -84,9 +92,40 @@ class ForeCastWeatherRemoteDataSourceImplTest {
         } throws UnknownHostException(output)
 
         foreCastWeatherRemoteDataSource.getForecastWeather(input)
-            .catch { error ->
-                Truth.assertThat(error.message).isEqualTo(output)
-                Truth.assertThat(error).isInstanceOf(UnknownHostException::class.java)
-            }.collect()
+                .catch { error ->
+                    Truth.assertThat(error.message).isEqualTo(output)
+                    Truth.assertThat(error).isInstanceOf(UnknownHostException::class.java)
+                }.collect()
+    }
+
+    @Test
+    fun `fetch list failed`() = coroutinesTestRule.runBlockingTest {
+
+        val input = LocationRequestModal("London")
+        val output = "Invalid data received"
+
+        val error = mockk<ResponseBody>(relaxed = true)
+
+        val id = slot<WillyWeatherResult<ForecastLocationEntity>>()
+
+        coEvery {
+            weatherApiService.foreCastWeather(any(), any(), any(), any()).toResult{
+                capture(id)
+            }
+        } returns  ErrorResult(message ="HI")
+
+
+        val resp = foreCastWeatherRemoteDataSource.getForecastWeather(input).single()
+        foreCastWeatherRemoteDataSource.getForecastWeather(input)
+                .catch { error ->
+                    Truth.assertThat(error.message).isEqualTo(output)
+                    Truth.assertThat(error).isInstanceOf(UnknownHostException::class.java)
+                }.collect {
+                    Truth.assertThat(it).isInstanceOf(ErrorResult::class.java)
+                    Truth.assertThat((it as ErrorResult).message).isEqualTo("dk")
+                }
+
+        Truth.assertThat(resp).isInstanceOf(ErrorResult::class.java)
+        Truth.assertThat(resp).isEqualTo(id.captured)
     }
 }
